dd2e2dc59962bb87492181a75d2f9afcc7029740
TRVevel <piscionequentin@gmail.com>
2024-12-12 12:22:19 +0100

    Add files via upload

---
+++ src/page2.js
@@ -0,0 +1,49 @@
+function getUserIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('userid');
+}
+function displayPosts(posts) {
+
+    postsContainer.innerHTML = '';
+    posts.forEach(async post => {
+        console.log(post)
+
+        let userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        let user = await userResponse.json();
+
+
+        const postElement = document.createElement('div');
+        postElement.className = 'post';
+        postElement.innerHTML = `
+        <p class="user-link">
+                <strong>Utilisateur : </strong><p>${user.name}</p>
+            </p>
+            <h2>titre:${post.title}</h2>
+            <p>${post.body}</p>
+        `;
+        postsContainer.appendChild(postElement);
+
+    });
+}
+
+
+async function fetchPosts() {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${userId}`);
+        const posts = await response.json();
+        displayPosts(posts);
+    } catch (error) {
+        postsContainer.innerHTML = `<p>${error}</p>`;
+    }
+
+}
+
+
+const userId = getUserIdFromUrl();
+const postsContainer = document.getElementById('posts');
+
+if (!userId) {
+    postsContainer.innerHTML = '<p>Error: User ID is not specified in the URL.</p>';
+} else {
+    fetchPosts();
+}
