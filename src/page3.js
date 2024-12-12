884a6346687f54e309c5349caa88b40fc2683be9
Quentin.Pi <piscionequentin@gmail.com>
2024-12-12 16:18:54 +0100

    Css presque Finis

--- src/page3.js
+++ src/page3.js
@@ -1,40 +1,38 @@
-
-
-
-
 async function fetchPosts() {
+    //Recuperation de la partie qui sert a recupere la donnée provenant de la page 1 (index.html) via le lien
     let queryString = window.location.search;
     console.log(queryString);
 
+    //Recuperation de la valeur de la donnée 
     let urlParams = new URLSearchParams(queryString);
-
     let postId = urlParams.get('postId')
     console.log(postId);
-
+    //Requete Get du post avec l'Id récuperer
     let postsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     let posts = await postsResponse.json();
     let postsContainer = document.getElementById('posts');
 
-   
-        let userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${posts.userId}`);
-        let user = await userResponse.json();
+    //Requete Get des informations de l'utilisateur avec le UserId récuperer dans le post
+    let userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${posts.userId}`);
+    let user = await userResponse.json();
 
-        // Creer un élément pour chaque post
-        let postDiv = document.createElement('div');
-        postDiv.classList.add('post');
-        let commentsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
-        let comments = await commentsResponse.json();
+    // Creer un element pour chaque post
+    let postDiv = document.createElement('div');
+    postDiv.classList.add('post');
+    //avec recuperation des commentaire via requete Get
+    let commentsResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+    let comments = await commentsResponse.json();
         
        
 
         // Ajouter le titre et le contenu du post
         postDiv.innerHTML = `
-        <p class="user-link">
-                <strong>Utilisateur : </strong><a href="./page2.html?userId=${posts.userId}" target="_blank">${user.name}</a>
-            </p>
-            <h3>Titre: ${posts.title}</h3>
-            <p>${posts.body}</p>  
-            <h3>Commentaires:</h3>
+          <p class="user-link">
+            <strong>Utilisateur : </strong><a href="./page2.html?userId=${posts.userId}" target="_blank">${user.name}</a>
+          </p>
+          <h3>Titre: ${posts.title}</h3>
+          <p>${posts.body}</p>  
+          <h3>Commentaires:</h3>
             `;
             for(let comment of comments)(
                 postDiv.innerHTML =postDiv.innerHTML + `
@@ -63,19 +61,9 @@ async function fetchPosts() {
             
             deleteButton.addEventListener("click",deletePost)
 
-            
-            
-           
-        
-        
         //Ajouter le post a la page
         postsContainer.appendChild(postDiv);
 
-        
- 
 }
-// let homeButton= document.getElementById("home");
-// homeButton.addEventListener("onClick", )
-// Appel de la fonction pour charger les posts
 fetchPosts();
 
